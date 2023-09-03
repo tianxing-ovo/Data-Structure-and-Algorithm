226ddf5101fda6b481156d6c8b6765fc46281d33
ltx <2050365570@qq.com>
2023-09-03 16:48:09 +0800

    选择排序

---
+++ src/main/java/排序/简单排序/选择排序.java
@@ -0,0 +1,35 @@
+package 排序.简单排序;
+
+import java.util.Arrays;
+
+import static 排序.Array.nums;
+import static 排序.Array.swap;
+
+/**
+ * 每一轮中选择未排序部分中的最小元素，将其放置在已排序部分的末尾
+ */
+public class 选择排序 {
+
+    /**
+     * 5, 3, 7, 2, 9, 8, 1, 4
+     */
+    public static void main(String[] args) {
+        selectionSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+
+    public static void selectionSort(int[] nums) {
+        int n = nums.length;
+        int min; // 最小元素索引
+        for (int i = 0; i < n - 1; i++) { // 第一个没有排序过的元素的索引
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            swap(nums, i, min);
+        }
+    }
+}
